487485476d9afcb7ce73785b52aefd055b42a076
iainb <ibroadfo@local>
2025-05-29 15:59:26 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	  import { MoonIcon, SunIcon } from "@lucide/svelte";
+</script>
+
+<SunIcon
+   class="h-[1.2rem] w-[1.2rem] duration-500 ease-in-out rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+  />
+  <MoonIcon
+   class="absolute h-[1.2rem] w-[1.2rem] duration-500 ease-in-out rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+  />
